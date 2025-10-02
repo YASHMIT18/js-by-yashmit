882f44c30b0b7b32af8e7905ed03c54ceede2ad6
Yashmit Lamba <lambayashmit@gmail.com>
2025-10-03 00:07:22 +0530

    Variables

---
+++ 02.variables.js
@@ -0,0 +1,29 @@
+"use strict";//treat all js code as new version
+
+// // alert(6+6)  we are using nodejs not browser
+// // console.log(6+6) we can add like this also
+
+// //code readablity is very important and very high in large projects
+
+// Datatypes in js
+
+// let name = "yashmit"; //string datatype use ""is better
+
+let age = 21; //number datatype the range of number in js is -2^53 to 2^53
+
+// let isLoggedIn = false; //boolean datatype true or false
+
+// let bigint = 1234567890123456789012345678901234567890n;//bigint datatype is used to store large number
+
+// let nullvar = null; //null datatype is used to store null value
+
+// let undef = undefined; //undefined datatype is used to store value which is not defined
+
+// let sym = Symbol("this is symbol");//symbol datatype is used to create unique identifiers
+
+// //object datatype is used to store key value pairs
+
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+   
