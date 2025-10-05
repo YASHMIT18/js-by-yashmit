a5af7fc5a87824947b95574862b2597057a61eba
Yashmit Lamba <lambayashmit@gmail.com>
2025-10-05 14:56:25 +0530

    conversions

---
+++ 03.conversionoperation.js
@@ -0,0 +1,24 @@
+// let name = "33ac"
+
+// console.log(typeof name)
+
+// let valueinnumber = Number(name)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)/
+
+
+// YOU CAN DO CONVERSION BY USING LET VALUEIN NUMBER OR STRING OR BOOLEAN
+// = --- IN WHICH DATATYPE YOU WANT TO CONVERT = --- IN WHICH DATATYPE YOU WANT TO 
+
+
+let isloggedin = "1"
+console.log(typeof isloggedin)
+
+let valueinnumber =Boolean(isloggedin)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+// in string is we write 1,2,3,4,5,6,7,8,9 it will be true
+// in string if we write 0 it will be false
+// in string if we write any number it will be true
+// in string if we write empty string it will be false
